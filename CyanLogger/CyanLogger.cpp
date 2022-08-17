bf115af89413a665c24ee8ac245341648d397cd2
Sammot <sammot@users.noreply.github.com>
2022-08-17 07:50:48 +0100

    Setting up project structure (git+msvc)

---
+++ CyanLogger/CyanLogger.cpp
@@ -0,0 +1,12 @@
+﻿// CyanLogger.cpp : Defines the entry point for the application.
+//
+
+#include "CyanLogger.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CyanLogger
  
+This is a C++17 logging library made for my personal use, but you can use it if you want!
